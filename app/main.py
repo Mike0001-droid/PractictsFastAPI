4f1a9ec3c7061b00408ca8b5136df7069a3655e1
Mikhail <majorov-2004@list.ru>
2025-04-25 17:10:49 +0500

    add task

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from database import engine
+from database import engine, clear_cache_daily
 from models import Base
 from routers import trading_dates, dynamics, trading_results
+import asyncio
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,6 +11,16 @@ app = FastAPI()
 app.include_router(trading_dates.router)
 app.include_router(dynamics.router)
 app.include_router(trading_results.router)
+
+async def run_periodically():
+    while True:
+        if clear_cache_daily():
+            print("Cache cleared at 14:11")
+        await asyncio.sleep(60)  
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(run_periodically())
 
 @app.get("/")
 def read_root():
